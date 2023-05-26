56ce852fd9a3117093397bd98ec3e07332ae1f1f
zhaojunming <zhaojunming@meituan.com>
2023-05-26 23:55:30 +0800

    feat: 支持阿里云TTS，抽取TTS相关Token和Key

--- src/hooks/useGetAudio.ts
+++ src/hooks/useGetAudio.ts
@@ -1,6 +1,6 @@
 import {useCallback, useState} from "react";
 import axios from "axios";
-import {MEITUAN_TTS_TOKEN} from "../configs";
+import {ALIYUN_TTS_APP_KEY, ALIYUN_TTS_TOKEN, MEITUAN_TTS_TOKEN} from "../configs";
 
 const blobToBase64 = (blob: any) => {
     return new Promise((resolve, _) => {
@@ -10,29 +10,54 @@ const blobToBase64 = (blob: any) => {
     });
 }
 
-export const useGetAudio = () => {
+const getAliyunTTSAudio = (content: string) => {
+    const format = 'wav'
+    const voice = 'qingqing'
+    const sample_rate = 16000;
+    const url = `https://nls-gateway-cn-shanghai.aliyuncs.com/stream/v1/tts`
+
+    const params = {
+        appkey: ALIYUN_TTS_APP_KEY,
+        token: ALIYUN_TTS_TOKEN,
+        format,
+        voice,
+        text: content,
+        sample_rate,
+        volume: 100
+    };
+
+    return axios.post(url, params, {responseType: 'arraybuffer',})
+}
+
+const getMeituanTTSAudio = (content: string) => {
+    const sid = new Date().getTime() + 'a_whw'
+    return axios.post('https://tts.vip.sankuai.com/api/v1/tts', {
+        session_id: sid,
+        text: content,
+        voice_name: 'meimiyu',
+        speed: 50,
+        volume: 50,
+        sample_rate: 16000,
+        audio_format: 'wav'
+    }, {
+        responseType: 'arraybuffer',
+        headers: {
+            Token: MEITUAN_TTS_TOKEN,
+            SessionID: sid
+        }
+    })
+}
+
+export const useGetAudio = (type: string = 'meituan') => {
 
     const [loading, setLoading] = useState(false)
 
     const runAsync = useCallback((chat: string) => {
-        const sid = new Date().getTime() + 'a_whw'
-        // 取音频文件成 arraybuffer
+
+        const getTTSAudio = type === 'aliyun' ? getAliyunTTSAudio : getMeituanTTSAudio
         setLoading(true)
-        return axios.post('https://tts.vip.sankuai.com/api/v1/tts', {
-            session_id: sid,
-            text: chat,
-            voice_name: 'meimiyu',
-            speed: 50,
-            volume: 50,
-            sample_rate: 16000,
-            audio_format: 'wav'
-        }, {
-            responseType: 'arraybuffer',
-            headers: {
-                Token: MEITUAN_TTS_TOKEN,
-                SessionID: sid
-            }
-        }).then((res) => {
+        // 取音频文件成 arraybuffer
+        return getTTSAudio(chat).then((res) => {
             setLoading(false)
             const blob = new Blob([res?.data], {type: "audio/wav"})
             return blobToBase64(blob);
