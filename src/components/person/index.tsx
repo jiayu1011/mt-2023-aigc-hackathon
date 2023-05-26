6db88d545a09024b5af023923c1c290334f5b8ed
zhaojunming <zhaojunming@meituan.com>
2023-05-26 16:35:29 +0800

    feat: 加入布局场景、人物、反馈、输入等组件

--- src/components/person/index.tsx
+++ src/components/person/index.tsx
@@ -1,16 +1,9 @@
-import {memo} from "react";
-import style from './index.module.scss'
-
-interface Props {
-
-}
-
-export const Person = memo((props: Props) => {
-    console.log(props)
+export const Person = () => {
 
     return (
-        <div className={style.container}>
-            Person
+        <div>
+
         </div>
     )
-})
+}
+
