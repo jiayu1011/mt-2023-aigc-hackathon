bf95a6a85b3612ac462c83ec5f949138832610d5
xinjiayu02 <xinjiayu02@meituan.com>
2023-05-26 20:36:18 +0800

    feat: Live2DModel重置动画

--- src/hooks/useLive2DModel.tsx
+++ src/hooks/useLive2DModel.tsx
@@ -12,8 +12,9 @@ export const useLive2DModel = () => {
 
     const [isMotionFinished, setIsMotionFinished] = useState<boolean>(true)
     const [isAudioFinished, setIsAudioFinished] = useState<boolean>(true)
+    const [needsResetMotion, setNeedsResetMotion] = useState<boolean>(false)
 
-    const initLive2DModel = async (pixiApp: PIXI.Application) => {
+    const init = async (pixiApp: PIXI.Application) => {
         if (!pixiApp) return
 
         const model = await Live2DModel.from('/Resources/Hiyori/Hiyori.model3.json')
@@ -42,52 +43,70 @@ export const useLive2DModel = () => {
         model.scale.set(scale * 1.6)
         model.position.set(pixiApp.view.width*0.4, pixiApp.view.height*0.85)
 
+        model.internalModel.motionManager.on('motionFinish', () => {
+            console.log('motion finish')
+            setIsMotionFinished(true)
+        })
+        if (model?.internalModel.motionManager.currentAudio) {
+            model.internalModel.motionManager.currentAudio.onended = () => {
+                console.log('audio finish')
+                setIsAudioFinished(true)
+            }
+        }
+
         setModel(model)
     }
 
     useEffect(() => {
-        if (!model) return
+        // 此处监听needsResetMotion，防止重置动画结束触发的无限循环
+        if (!isMotionFinished || !isAudioFinished || !needsResetMotion) return
 
-        model.internalModel.motionManager.once('motionFinish', () => {
-            console.log('motionFinish')
-            setIsMotionFinished(true)
-        })
-    }, [model])
+        triggerResetMotion()
+    }, [isMotionFinished, isAudioFinished, needsResetMotion])
 
 
-    useEffect(() => {
-        if (!isMotionFinished || !isAudioFinished) return
+    const triggerResetMotion = () => {
+        /**
+         * 触发重置动画（Flat）
+         */
+        setNeedsResetMotion(false)
+        model.motion('Flat')
+    }
 
-        motion('Flat')
-    }, [isMotionFinished, isAudioFinished])
-
-
-    const stopAllMotion = () => {
+    const stopAllMotion = useCallback(() => {
         if (!model) return
         model.internalModel.motionManager.stopAllMotions()
-    }
+    }, [model])
 
-    const motion = useCallback( (type: MotionType) => {
+    const motion = useCallback((type: MotionType) => {
+        /**
+         * 调用动画
+         */
         if (!model) return
 
         setIsMotionFinished(false)
+        setNeedsResetMotion(true)
         stopAllMotion()
         model.motion(type)
-    },[model])
+    }, [model])
 
-    const motionWithAudio = (type: MotionType, audioB64: ArrayBuffer) => {
+    const motionWithAudio = useCallback((type: MotionType, audioB64: ArrayBuffer) => {
+        /**
+         * 调用带声音的动画，格式为wav或base64
+         */
         if (!model) return
 
         setIsMotionFinished(false)
+        setIsAudioFinished(false)
+        setNeedsResetMotion(true)
         stopAllMotion()
         model.motion(type, 0, MotionPriority.NORMAL, audioB64)
-    }
+    }, [model])
 
     return {
-        init:initLive2DModel,
+        init,
         motion,
         motionWithAudio,
         stopAllMotion,
-        model
     }
 }
